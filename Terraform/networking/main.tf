a7096b980a88b1f570b5c466266b9d87b5ac5671
kallada2021 <karthikeyaallada77@gmail.com>
2022-06-20 10:01:28 -0400

    Added Routes and Routetable.

--- Terraform/networking/main.tf
+++ Terraform/networking/main.tf
@@ -45,9 +45,18 @@ resource "aws_security_group" "PortifolioAllowTls" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "SSH from VPC"
+    description      = "HTTPS from VPC"
     from_port        = 443
     to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+   ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
@@ -65,3 +74,37 @@ resource "aws_security_group" "PortifolioAllowTls" {
     Name = "PortifolioAllowSshSecurityGroup"
   }
 }
+
+resource "aws_route_table" "portifolio-public-rt" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "portifolio-public-rt"
+  }
+}
+
+resource "aws_route" "portifolio-public-route" {
+  route_table_id         = aws_route_table.portifolio-public-rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.PortifolioGW.id
+}
+
+resource "aws_route_table_association" "portifolio-rt-assoc" {
+  count = 2
+  subnet_id      = aws_subnet.PublicSubnet[count.index].id
+  route_table_id = aws_route_table.portifolio-public-rt.id
+}
+
+/* resource "aws_nat_gateway" "portifolio-nat-gateway" {
+  allocation_id = aws_eip.example.id
+  subnet_id     = aws_subnet.example.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.example]
+} */
+
