a16a5cf34da5d1acbf64b731c73434adab01131d
kallada2021 <karthikeyaallada77@gmail.com>
2022-06-25 09:41:44 -0400

    Flutter Added Dropdown Widgets

--- consulting_portfolio_website/lib/constants/constants.dart
+++ consulting_portfolio_website/lib/constants/constants.dart
@@ -22,7 +22,8 @@ var webHomeScreenItems = [
   const SizedBox(
     width: GlobalVariables.lineWidth,
   ),
-  const Text("Services"),
+  DropdownMenuItem(child: const Text("Services")),
+  CustomDropdownItem(),
   const SizedBox(
     width: GlobalVariables.lineWidth,
   ),
@@ -35,3 +36,47 @@ var webHomeScreenItems = [
     width: GlobalVariables.lineWidth,
   ),
 ];
+
+List<DropdownMenuItem<String>> get dropdownItems{
+  List<DropdownMenuItem<String>> menuItems = [
+    DropdownMenuItem(child: Text("CloudInfrastructureDevelopment"),value: "CloudInfrastructureDevelopment"),
+    DropdownMenuItem(child: Text("WebDevelopment"),value: "WebDevelopment"),
+    DropdownMenuItem(child: Text("MobileDevelopment"),value: "MobileDevelopment"),
+    DropdownMenuItem(child: Text("Devops"),value: "Devops"),
+  ];
+  return menuItems;
+}
+
+List<DropdownMenuItem<String>> get dropdownItemsTech{
+  List<DropdownMenuItem<String>> menuItems = [
+    DropdownMenuItem(child: Text("Terraform"),value: "Terraform"),
+    DropdownMenuItem(child: Text("Packer"),value: "Packer"),
+    DropdownMenuItem(child: Text("Django"),value: "Django"),
+    DropdownMenuItem(child: Text("PowerShell"),value: "PowerShell"),
+    DropdownMenuItem(child: Text("BashScripting"),value: "Bashscripting"),
+    DropdownMenuItem(child: Text("Python"),value: "Python"),
+    DropdownMenuItem(child: Text("Flutter"),value: "Flutter"),
+    DropdownMenuItem(child: Text("GoLang"),value: "GoLang"),
+    DropdownMenuItem(child: Text("PowerPlatform"),value: "PowerPlatform")
+
+  ];
+  return menuItems;
+}
+
+class CustomDropdownItem extends StatefulWidget {
+  const CustomDropdownItem({Key? key}) : super(key: key);
+
+  @override
+  State<CustomDropdownItem> createState() => _CustomDropdownItemState();
+}
+
+class _CustomDropdownItemState extends State<CustomDropdownItem> {
+  String value = "Services";
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+        items: dropdownItems,
+        value: value,
+        onChanged: (_){});
+  }
+}
