7504c349268fc3f8df9cb5ef7b9f4b39e94562ac
Darin Milner <darinmcodingprojects@gmail.com>
2022-08-19 00:13:53 +0600

    Route53 code added

--- Terraform/Infrastructure/alb/main.tf
+++ Terraform/Infrastructure/alb/main.tf
@@ -1,12 +1,12 @@
-esource "aws_security_group" "portfolio-sg" {
+resource "aws_security_group" "portfolio-sg" {
   name        = "portfolio-sg"
   description = "Allow TLS inbound and outbound traffic"
   vpc_id      = var.vpc-id
 
   ingress {
     description = "HTTP from ALB"
-    from_port   = 80
-    to_port     = 80
+    from_port   = var.http_port
+    to_port     = var.http_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -35,7 +35,7 @@ esource "aws_security_group" "portfolio-sg" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow-tls"
   }
 }
 
@@ -52,20 +52,20 @@ resource "aws_lb" "portfolio-alb" {
 }
 
 resource "aws_lb_target_group" "portfolio-alb-tg" {
-  name = "portfolio-tg"
-  port = local.http_port
+  name     = "portfolio-tg"
+  port     = local.http_port
   protocol = local.http_protocol
-  vpc_id = var.vpc_id
+  vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_listener" "portfolio-listener" {
   load_balancer_arn = aws_lb.portfolio-alb.arn
-  port = local.https_port
-  protocol = local.https_protocol
-  ssl_policy = var.ssl_policy
-  certificate_arn = var.certificate_arn
+  port              = local.https_port
+  protocol          = local.https_protocol
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
   default_action {
-    type = var.listener_type
+    type             = var.listener_type
     target_group_arn = aws_lb_target_group.portfolio-alb-tg.arn
   }
   depends_on = [aws_lb.portfolio-alb, aws_lb_target_group.portfolio-alb-tg]
