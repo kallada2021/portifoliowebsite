651500e73bb93435c8e32efa9f11690461f1f7b1
Darin Milner <darinmcodingprojects@gmail.com>
2022-09-05 22:40:19 +0300

    Example Terraform test added

---
+++ Terraform/Infrastructure/test_infra.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import pytest
+import tftest
+
+
+@pytest.fixture
+def plan():
+    file_path = Path(__file__).resolve()
+    print(file_path)
+    base_dir = file_path.parent.absolute()
+
+    print(base_dir)
+    tf = tftest.TerraformTest(tfdir="./", basedir=base_dir)
+    # tf = tftest.TerraformTest("plan",fixtures_dir)
+    # tf.setup()
+    tf.init()
+    # print(tf)
+    return tf.plan(output=True)
+
+
+def test_modules(plan):
+    mod = plan.modules["module.networking"]
+    res = mod.resources["aws_vpc.main"]
+    print(res)
