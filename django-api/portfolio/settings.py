4419efe64cf4c28caebef9bd8e3a6df1b7e317a1
Karthikeya Allada <karthikeyaallada77@gmail.com>
2022-09-13 08:05:04 -0400

    Added description.

--- django-api/portfolio/settings.py
+++ django-api/portfolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ["localhost"]
 
 CORS_ALLOW_ALL_ORIGINS: True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:60035"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:56521"]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
