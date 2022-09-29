e766f1f47798e75f8b03a81b17c9bbac54b6b6a3
Darin Milner <darinmcodingprojects@gmail.com>
2022-09-29 23:22:06 +0300

    Changed Packer and Django workflows

--- Terraform/packer/install-docker.sh
+++ Terraform/packer/install-docker.sh
@@ -20,16 +20,24 @@ sudo usermod -aG docker ubuntu
 sudo docker run hello-world
 
 #install aws-cli
-sudo apt-get install -y awscli
+# sudo apt-get install -y awscli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+# install aws-cli with pip3
+# sudo apt install python3-pip -y
+# pip3 install awscli --upgrade --user
 export AWS_ACCESS_KEY_ID=$ACCESSKEY 
 export AWS_SECRET_ACCESS_KEY=$SECRETKEY
 
 # ECR login
-aws ecr get-login-password --region $aws-region | docker login --username $USERNAME --password-stdin $ACCOUNTID.dkr.ecr.$REGION.amazonaws.com/$ECRREPO 
+aws ecr get-login-password --region $aws-region | sudo docker login --username $USERNAME --password-stdin $ACCOUNTID.dkr.ecr.$REGION.amazonaws.com/$ECRREPO 
+# docker login -u $USERNAME -p $(aws ecr get-login-password --region $REGION) $ACCOUNTID.dkr.ecr.$REGION.amazonaws.com/$ECRREPO
 
 #Pull docker containers from ECR
 docker pull $ECR_REGISTRY/$ECRREPO:Django-$IMAGE_TAG
 docker pull $ECR_REGISTRY/$ECRREPO:Nginx-$IMAGE_TAG
 docker network create portfolio
-docker run --name api --network portfolio $ECR_REGISTRY/$ECRREPO:Django-$IMAGE_TAG
-docker run -p 8080:80 --name nginx --network portfolio $ECR_REGISTRY/$ECRREPO:Nginx-$IMAGE_TAG
+docker run --name api --network portfolio $ECR_REGISTRY/$ECR_REPOSITORY:django-latest
+docker run -p 8080:80 --name nginx --network portfolio $ECR_REGISTRY/$ECRREPO:nginx-latest 
