8c48c9820abc93b677927b1b62c25ece1602c29e
Karthikeya Allada <karthikeyaallada77@gmail.com>
2022-10-11 11:18:54 -0400

    fixed packer variable.

--- Terraform/Infrastructure/compute/run-docker.sh
+++ Terraform/Infrastructure/compute/run-docker.sh
@@ -2,7 +2,9 @@
 
 cd portifoliowebsite
 cd django-api  
-aws secretsmanager get-secret-value --secret-id $DB_SECRET --region $REGION | \
+#$DB_SECRET
+#$REGION
+aws secretsmanager get-secret-value --secret-id $DB_SECRET--region $REGION| \
             jq -r '.SecretString' | \
             jq -r "to_entries|map(\"\(.key)=\\\"\(.value|tostring)\\\"\")|.[]" > .env
 docker-compose up -d 
