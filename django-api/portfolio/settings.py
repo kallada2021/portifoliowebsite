0db9f74b0ee6782dab6a734922e7682f74f3c8df
Karthikeya Allada <karthikeyaallada77@gmail.com>
2022-10-11 12:44:47 -0400

    fixed packer variable.

--- django-api/portfolio/settings.py
+++ django-api/portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "https://magnoliaitconsolting.com"]
+ALLOWED_HOSTS = ["localhost", "https://magnoliaitconsolting.com","10.0.1.209"]
 
 
 CORS_ALLOW_ALL_ORIGINS: True
