c33158e77853008fe8970edf42454ef0b66aee09
Darin Milner <darinmcodingprojects@gmail.com>
2022-10-12 14:32:01 +0300

    Django settings updated

--- django-api/portfolio/settings.py
+++ django-api/portfolio/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "https://magnoliaitconsolting.com", "*"]
+ALLOWED_HOSTS = ["localhost", "https://magnoliaitsolutionsgroup.com", "*"]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080", "https://magnoliaitsolutionsgroup.com"]
 
 CORS_ALLOW_ALL_ORIGINS: True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "https://magnoliaitconsolting.com"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "https://magnoliaitsolutionsgroup.com"]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
