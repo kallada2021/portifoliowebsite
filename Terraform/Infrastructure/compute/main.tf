567ec80b61b3a805f0479062342e95c517d18dea
Darin Milner <darinmcodingprojects@gmail.com>
2022-10-12 14:47:29 +0300

    AMI updated

--- Terraform/Infrastructure/compute/main.tf
+++ Terraform/Infrastructure/compute/main.tf
@@ -119,7 +119,7 @@ resource "aws_iam_policy" "ec2-policy" {
 // Provision ec2
 resource "aws_instance" "webserver" {
   #ami = "ami-052efd3df9dad4825"
-  ami                    = "ami-068735ea5544176e7"
+  ami                    = "ami-0f4728ccff745db48"
   instance_type          = var.instance-type
   iam_instance_profile   = aws_iam_instance_profile.ec2-profile.name
   key_name               = "portfolioec2user"
